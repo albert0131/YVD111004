6dfb309d12d12861efa5bbe3df4786e34e542c3f
albert <alber@gmail.com>
2016-11-10 15:39:26 +0800

    SimpleAdapter

--- app/src/main/java/com/example/user/yvd111004/MainActivity.java
+++ app/src/main/java/com/example/user/yvd111004/MainActivity.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] fruits = {"蘋果", "鳳梨", "芭樂", "香蕉"};
+    String[] cities = {"台北", "台中", "台南", "高雄"};
+    String[] codes = {"02", "04", "06", "07"};
+    ArrayList<Map<String, String>> mylist;
+
     ListView lv;
     ArrayAdapter<String> adapter;
 
@@ -17,8 +26,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lv = (ListView) findViewById(R.id.listView);
-        adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,fruits);
+//        adapter = new ArrayAdapter<String>(MainActivity.this,
+//                android.R.layout.simple_list_item_1,fruits);
+
+        mylist = new ArrayList<>();
+        for (int i=0;i<cities.length;i++)
+        {
+            Map m = new HashMap();
+            m.put("city", cities[i]);
+            m.put("code", codes[i]);
+            mylist.add(m);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
+                mylist, android.R.layout.simple_list_item_2,
+                new String[] {"city", "code"},
+                new int[] {android.R.id.text1, android.R.id.text2});
+
+
         lv.setAdapter(adapter);
 
     }
